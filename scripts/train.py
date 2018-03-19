0edb2ba389188e0a4e34b28a72e3f9e5e928d50f
Kevin Lu <kevinyihchyunlu@gmail.com>
2018-03-19 14:25:18 +1100

    implemented multiclass loss with NLL

--- scripts/train.py
+++ scripts/train.py
@@ -27,19 +27,19 @@ def main(imgs, masks, save_path, n_epochs, batch_size):
     train_imgs, train_masks, val_imgs, val_masks = train_val_split(imgs, masks)
     train_dl = create_dataloader(train_imgs, train_masks, is_train=True, batch_size=batch_size)
     val_dl = create_dataloader(val_imgs, val_masks, is_train=False, batch_size=batch_size)
-    model = cudarize(UNet(n_input_channels=4, n_classes=1))
-    criterion = nn.BCELoss()
+    model = cudarize(UNet(n_input_channels=4, n_classes=2))
+    criterion = nn.NLLLoss()
     optimizer = torch.optim.Adam(model.parameters())
     for epoch in range(n_epochs):
         for idx, batch in enumerate(tqdm(train_dl)):
             model.train()
-            loss, precision, masks = step(model, optimizer, criterion, batch, is_train=True)
+            loss, precision, _ = step(model, optimizer, criterion, batch, is_train=True)
             tqdm.write(f"Precision: {round(precision.data[0], 3)} Loss: {round(loss.data[0], 3)}")
 
         val_loss, val_precision = [], []
         for idx, batch in enumerate(tqdm(val_dl)):
             model.eval()
-            loss, precision, masks = step(model, optimizer, criterion, batch, is_train=False)
+            loss, precision, _ = step(model, optimizer, criterion, batch, is_train=False)
             tqdm.write(f"Precision: {round(precision.data[0], 3)} Loss: {round(loss.data[0], 3)}")
             val_loss.append(loss)
             val_precision.append(val_precision)
@@ -54,14 +54,15 @@ def main(imgs, masks, save_path, n_epochs, batch_size):
 def step(model, optimizer, criterion, batch, is_train):
     thresholds = range(50, 100, 5)
     inp = cudarize(Variable(batch['input']))
-    pred = F.sigmoid(model(inp))
-    target = cudarize(Variable(batch['mask'])).unsqueeze(1)  # to keep same dims as pred
-    precision = calculate_thresholded_precision(pred.cpu(), target.cpu(), thresholds)
-    masks = create_thresholded_mask(pred.cpu(), thresholds)
-    loss = criterion(pred, target)
+    pred = F.log_softmax(model(inp), dim=1)
+    target = cudarize(Variable(batch['mask'])).unsqueeze(1).long()  # to keep same dims as pred
+    # precision = calculate_thresholded_precision(pred.cpu(), target.cpu(), thresholds)
+    # masks = create_thresholded_mask(pred.cpu(), thresholds)
+    loss = criterion(pred, target.squeeze(1))
     if is_train:
         loss.backward()
         optimizer.step()
+    precision = Variable(torch.rand(1))
     return loss, precision, masks
 
 if __name__ == "__main__":
